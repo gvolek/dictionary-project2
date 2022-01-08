df171fb2e5694a5b7b8d00c2f6fe819aa18d1a81
Gema R Volek <gemarubycastro@gmail.com>
2022-01-07 20:09:08 -0700

    Created CSS & useState for keyword

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import gemaimg from "./gemaimg.jpg";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={gemaimg} className="Gema-img img-fluid" alt="gema img" />
+      <div className="container">
+        <header className="App-header">
+          <img src={gemaimg} className="Gema-img img-fluid" alt="gema img" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Gema Volek</small>
+        </footer>
+      </div>
     </div>
   );
 }
